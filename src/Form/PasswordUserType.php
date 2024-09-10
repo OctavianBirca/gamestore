e90a4fe71bee8981c913f93eeabcb2acea963f43
Octavian Birca <octavian.birca@gmail.com>
2024-09-10 18:25:02 +0200

    Uploaded files

---
+++ src/Form/PasswordUserType.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class PasswordUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('actualPassword', PasswordType::class, [
+                    'label' => "Votre mot de passe actuel",
+                    'attr' => [
+                        'placeholder' => "Votre mot de passe actuel",
+                    ],
+                    'mapped' => false 
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new Length([
+                        'min' => 7,
+                        'max' => 35
+                    ])
+                ],
+                'first_options'  => [
+                    'label' => 'Votre mot de passe',
+                    'attr' => [
+                        'placeholder' => "Choisissez votre nouveau mot de passe"
+                    ], 
+                    'hash_property_path' => 'password'
+                ],
+                'second_options' => [
+                    'label' => 'Confirme votre mot de passe',
+                    'attr' => [
+                        'placeholder' => "Confirmez votre mot de passe"
+                    ]
+                ],
+                'mapped' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Valider", 
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+            ->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+                $form = $event->getForm();
+                $user = $form->getConfig()->getOptions()['data'];
+                $passwordHasher = $form->getConfig()->getOptions()['passwordHasher'];
+                
+                
+
+                $isValid = $passwordHasher->isPasswordValid(
+                    $user,
+                    $form->get('actualPassword')->getData()
+                );
+
+                if(!$isValid) {
+                    $form->get('actualPassword')->addError(new FormError("Votre mot passe est ne correspondent pas"));
+                }
+
+        
+
+                
+            });
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'passwordHasher' => null
+        ]);
+    }
+}
